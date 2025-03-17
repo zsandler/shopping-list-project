ee7d290a8aafea3324286262aa96f6d996f41da4
Zak Sandler <zaksandler@gmail.com>
2025-03-17 14:50:59 -0400

    functional code with styling!

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Grocery(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    def clean(self):
+        self.name = self.name.capitalize()
     quantity = models.IntegerField()
 
     def __str__(self):
